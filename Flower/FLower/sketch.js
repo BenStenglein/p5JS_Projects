f05a580872db2148968e8fea5a734ed93b15e789
BenStenglein <159486690+BenStenglein@users.noreply.github.com>
2026-02-25 18:31:08 +0000

    made beizer work

--- Flower/FLower/sketch.js
+++ Flower/FLower/sketch.js
@@ -31,7 +31,6 @@ let dirt = [
     ]
 let leafUnit = 0.25*unit
 let leafBack = [
-    
     {x:leafUnit,y:-leafUnit},
     {x:3*leafUnit,y:-leafUnit},
     {x:4*leafUnit,y:0},
@@ -45,12 +44,15 @@ let testCircle = [
 ]
 let leaf = [
     {x:0,y:0},
-    {x:4,y:0}
+    // {x:4,y:0}
+    {x1:0,y1:0,x2:2,y2:1,x3:4,y3:0},
+    {x1:4,y1:0,x2:2,y2:-1,x3:0,y3:0}
 ]
 function setup() {
     // pot = scalePolygon(pot,1)
     pot = translatePolygon(pot,0,unit)
     leaf = scalePolygon(leaf,unit)
+    
     pot = scalePolygon(pot,1);
     dirt = scalePolygon(dirt,2);
     createCanvas(800, 600); 
@@ -62,9 +64,9 @@ function draw() {
     // scale
     // drawPolygon(dirt,"#54392D")
     drawComplexPolygon(pot,"#E35336",0)
-    
+    drawComplexPolygon(leaf,"#000",1)
     // drawPolygon(zeroPot,"#fff")
-    drawComplexPolygon(testCircle,"#ff0000",0)
+    // drawComplexPolygon(testCircle,"#ff0000",0)
 }
 function translatePolygon(arr,dx,dy){
     newArr = []
@@ -78,33 +80,45 @@ function translatePolygon(arr,dx,dy){
 function scalePolygon(arr,factor){
     newArr = []
     for(let i =0; i<arr.length;i++){
-        let nX = arr[i].x *= factor;
-        let nY = arr[i].y *= factor;
-        newArr.push({x:nX,y:nY});
+        if(Object.keys(arr[i]).length == 2){
+            let nX = arr[i].x *= factor;
+            let nY = arr[i].y *= factor;
+            newArr.push({x:nX,y:nY});
+        }
+        else if(Object.keys(arr[i]).length == 6){
+            let nX1 = arr[i].x1 *= factor;
+            let nY1 = arr[i].y1 *= factor;
+            let nX2 = arr[i].x2 *= factor;
+            let nY2 = arr[i].y2 *= factor;
+            let nX3 = arr[i].x3 *= factor;
+            let nY3 = arr[i].y3 *= factor;
+            newArr.push({x1:nX1,y1:nY1,x2:nX2,y2:nY2,x3:nX3,y3:nY3});
+        }
     }
-    return newArr
+    return newArr;
 }
 function drawComplexPolygon(arr,color,stroke){
     strokeWeight(stroke);
-    fill(color)
-    beginShape()
+    fill(color);
+    beginShape();
     for(let i =0; i<arr.length;i++){
         if(Object.keys(arr[i]).length == 2){
-            // noFill()
             let x = arr[i].x
             let y = arr[i].y
             vertex(x,y)
         }
         else if(Object.keys(arr[i]).length == 6){
-            noFill()
-            let x1 = arr[i].x1
-            let y1 = arr[i].y1
-            let x2 = arr[i].x2
-            let y2 = arr[i].y2
-            let x3 = arr[i].x3
-            let y3 = arr[i].y3
-            bezierVertex(x1,y1,x2,y2,x3,y3)
+            console.log("hit")
+            let nX1 = arr[i].x1
+            let nY1 = arr[i].y1
+            let nX2 = arr[i].x2
+            let nY2 = arr[i].y2
+            let nX3 = arr[i].x3
+            let nY3 = arr[i].y3
+            bezierVertex(nX1,nY1);
+            bezierVertex(nX2,nY2);
+            bezierVertex(nX3,nY3);
         }
     }
-    endShape(CLOSE);
+    endShape();
 }
