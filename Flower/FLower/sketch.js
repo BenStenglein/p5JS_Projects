2421e27ebc118222d6527a4dd9c2e5ac04a56c74
BenStenglein <159486690+BenStenglein@users.noreply.github.com>
2026-02-26 15:27:20 +0000

    begun work on centerpointing for bezier polygons

--- Flower/FLower/sketch.js
+++ Flower/FLower/sketch.js
@@ -45,14 +45,14 @@ let testCircle = [
 let leaf = [
     {x:0,y:0},
     // {x:4,y:0}
-    {x1:0,y1:0,x2:2,y2:1,x3:4,y3:0},
-    {x1:4,y1:0,x2:2,y2:-1,x3:0,y3:0}
+    {x1:2,y1:0.5,x2:2,y2:0.5,x3:4,y3:0},
+    {x1:2,y1:-0.5,x2:2,y2:-0.5,x3:0,y3:0}
 ]
 function setup() {
     // pot = scalePolygon(pot,1)
     pot = translatePolygon(pot,0,unit)
     leaf = scalePolygon(leaf,unit)
-    
+    leaf = translatePolygon(leaf,0,32)
     pot = scalePolygon(pot,1);
     dirt = scalePolygon(dirt,2);
     createCanvas(800, 600); 
@@ -63,19 +63,35 @@ function draw() {
     background(210)
     // scale
     // drawPolygon(dirt,"#54392D")
-    drawComplexPolygon(pot,"#E35336",0)
-    drawComplexPolygon(leaf,"#000",1)
+    // approxRect(leaf)
+    // drawComplexPolygon(pot,"#E35336",0)
+    // drawComplexPolygon(leaf,"#000",1)
     // drawPolygon(zeroPot,"#fff")
     // drawComplexPolygon(testCircle,"#ff0000",0)
+    testfunc()
+    // console.log(leaf[1].x3)
+    
 }
 function translatePolygon(arr,dx,dy){
     newArr = []
     for(let i =0; i<arr.length;i++){
-        let nX = arr[i].x += dx;
-        let nY = arr[i].y += dy;
-        newArr.push({x:nX,y:nY});
+        if(Object.keys(arr[i]).length == 2){
+            let nX = arr[i].x += dx;
+            let nY = arr[i].y += dy;
+            newArr.push({x:nX,y:nY});
+        }
+        else if(Object.keys(arr[i]).length == 6){
+            let nX1 = arr[i].x1 += dx;
+            let nX2 = arr[i].x2 += dx;
+            let nX3 = arr[i].x3 += dx;
+
+            let nY1 = arr[i].y1 += dy;
+            let nY2 = arr[i].y2 += dy;
+            let nY3 = arr[i].y3 += dy;
+            newArr.push({x1:nX1,y1:nY1,x2:nX2,y2:nY2,x3:nX3,y3:nY3});
+        }
     }
-    return newArr
+    return newArr;
 }
 function scalePolygon(arr,factor){
     newArr = []
@@ -108,7 +124,6 @@ function drawComplexPolygon(arr,color,stroke){
             vertex(x,y)
         }
         else if(Object.keys(arr[i]).length == 6){
-            console.log("hit")
             let nX1 = arr[i].x1
             let nY1 = arr[i].y1
             let nX2 = arr[i].x2
@@ -122,3 +137,115 @@ function drawComplexPolygon(arr,color,stroke){
     }
     endShape();
 }
+function approxRect(arr){
+    let x1 = arr[0].x;
+    let y1 =arr[0].y;
+    let x2 = x1++;
+    let y2 =y1++;
+    beginShape()
+    fill("#fff")
+    stroke(5)
+    for(let i =0; i<arr.length;i++){
+        if(Object.keys(arr[i]).length == 2){
+            let x =arr[i].x;
+            let y = arr[i].y;
+            if (x < x1 || x > x2){
+                if (x > x2){
+                    x2 = x;
+                }
+                else if(x <x1){
+                    x1 = x;
+                }
+            }          
+            if (y < y1 || y > y2){
+                if (y > y2){
+                        y2 = y;
+                }
+                else if(y <y1){
+                    y1 = y;
+                }
+            }
+        }
+        else if(Object.keys(arr[i]).length == 6){
+            let nX1 = (Object.keys(arr[i-1]).length == 6) ? arr[i-1].x3 : arr[i-1].x;
+            let nX2 = arr[i].x1
+            let nX3 = arr[i].x2
+            let nX4 = arr[i].x3
+
+            let nY1 = (Object.keys(arr[i-1]).length == 6) ? arr[i-1].y3 : arr[i-1].y;
+            let nY2 = arr[i].y1
+            let nY3 = arr[i].y2
+            let nY4 = arr[i].y3
+
+            let x = bezierPoint(nX1,nX2, nX3, nX4, 0.5);
+            let y = bezierPoint(nY1,nY2, nY3, nY4, 0.5);
+            if (x < x1 || x > x2){
+                if (x > x2){
+                    x2 = x;
+                }
+                else if(x <x1){
+                    x1 = x;
+                }
+            }
+            else{
+                console.log("intial x failed, trying anchor 2")
+                x = nX4;
+                if (x != 0){
+                    console.log(x)
+                }
+                if (x < x1 || x > x2){
+                if (x > x2){
+                    x2 = x;
+                    console.log("using anchor 2 as x2")
+                }
+                else if(x <x1){
+                    x1 = x;
+                    console.log("using anchor 2 as x1")
+                }
+            }
+            }   
+            if (y < y1 || y > y2){
+                if (y > y2){
+                        y2 = y;
+                }
+                else if(y <y1){
+                    y1 = y;
+                }
+            }
+            else{
+                y = nY4;
+                if (y < y1 || y > y2){
+                if (y > y2){
+                    y2 = y;
+                }
+                else if(y <y1){
+                    y1 = y;
+                }
+            }
+            }  
+        }
+        
+    }
+    vertex(x1,y1)
+    vertex(x1,y2)
+    vertex(x2,y2)
+    vertex(x2,y1)
+    endShape(CLOSE)
+    let centX = x1+((x2-x1)/2)
+    let centY = y1+((y2-y1)/2)
+    strokeWeight(3)
+    point(centX,centY)
+}
+function testfunc(){
+   
+}
+/* useful logic structure for iterating through polygons
+for(let i =0; i<arr.length;i++){
+        if(Object.keys(arr[i]).length == 2){
+                
+        }
+        else if(Object.keys(arr[i]).length == 6){
+
+        }
+}
+*/
